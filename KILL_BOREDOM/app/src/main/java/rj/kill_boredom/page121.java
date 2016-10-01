e5771e62b165bfa55e9c1b0db2ee84f15b4a9821
romalrj <romalrj07@gmail.com>
2016-10-02 04:58:55 +0530

    change 6

--- KILL_BOREDOM/app/src/main/java/rj/kill_boredom/page121.java
+++ KILL_BOREDOM/app/src/main/java/rj/kill_boredom/page121.java
@@ -1,13 +1,32 @@
 package rj.kill_boredom;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class page121 extends AppCompatActivity {
+
+    private static Button open1211;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page121);
+        clickopen1211();
+    }
+
+    public void clickopen1211() {
+        open1211=(Button)findViewById(R.id.bopen1211);
+        open1211.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        Intent change= new Intent("rj.kill_boredom.page1211");
+                        startActivity(change);
+                    }
+                }
+        );
     }
 }
---
+++ KILL_BOREDOM/app/src/main/java/rj/kill_boredom/page1211.java
@@ -0,0 +1,13 @@
+package rj.kill_boredom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class page1211 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page1211);
+    }
+}
