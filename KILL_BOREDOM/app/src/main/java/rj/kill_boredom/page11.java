f1803c7ec81f295e465f3dd8da4d36418d33c1e8
romalrj <romalrj07@gmail.com>
2016-10-02 04:26:34 +0530

    change 5

--- KILL_BOREDOM/app/src/main/java/rj/kill_boredom/page11.java
+++ KILL_BOREDOM/app/src/main/java/rj/kill_boredom/page11.java
@@ -1,13 +1,48 @@
 package rj.kill_boredom;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class page11 extends AppCompatActivity {
+
+    private static Button open111;
+    private static Button open112;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page11);
+        clickopen111();
+        clickopen112();
+    }
+
+    public void clickopen111() {
+        open111=(Button)findViewById(R.id.bopen111);
+        open111.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        Intent change= new Intent("rj.kill_boredom.page111");
+                        startActivity(change);
+                    }
+                }
+        );
+    }
+
+
+    public void clickopen112() {
+        open112=(Button)findViewById(R.id.bopen112);
+        open112.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        Intent change= new Intent("rj.kill_boredom.page112");
+                        startActivity(change);
+                    }
+                }
+        );
     }
 }
--- KILL_BOREDOM/app/src/main/java/rj/kill_boredom/page12.java
+++ KILL_BOREDOM/app/src/main/java/rj/kill_boredom/page12.java
@@ -1,13 +1,32 @@
 package rj.kill_boredom;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class page12 extends AppCompatActivity {
+
+    private static Button open121;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page12);
+        clickopen121();
+    }
+
+    public void clickopen121() {
+        open121=(Button)findViewById(R.id.bopen121);
+        open121.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        Intent change= new Intent("rj.kill_boredom.page121");
+                        startActivity(change);
+                    }
+                }
+        );
     }
 }
