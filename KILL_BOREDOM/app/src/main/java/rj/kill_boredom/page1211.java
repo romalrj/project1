4639832982e02707dec1f5a6498726ed82cf04db
romalrj <romalrj07@gmail.com>
2016-10-02 06:23:18 +0530

    change 7

--- KILL_BOREDOM/app/src/main/java/rj/kill_boredom/page1211.java
+++ KILL_BOREDOM/app/src/main/java/rj/kill_boredom/page1211.java
@@ -2,6 +2,9 @@ package rj.kill_boredom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class page1211 extends AppCompatActivity {
 
@@ -9,5 +12,52 @@ public class page1211 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page1211);
+    }
+
+
+    public void oncheckClick(View v) {
+        EditText n11 = (EditText)findViewById(R.id.n11);
+        EditText n12 = (EditText)findViewById(R.id.n12);
+        EditText n13 = (EditText)findViewById(R.id.n13);
+        EditText n14 = (EditText)findViewById(R.id.n14);
+        EditText n21 = (EditText)findViewById(R.id.n21);
+        EditText n22 = (EditText)findViewById(R.id.n22);
+        EditText n23 = (EditText)findViewById(R.id.n23);
+        EditText n24 = (EditText)findViewById(R.id.n24);
+        EditText n31 = (EditText)findViewById(R.id.n31);
+        EditText n32 = (EditText)findViewById(R.id.n32);
+        EditText n33 = (EditText)findViewById(R.id.n33);
+        EditText n34 = (EditText)findViewById(R.id.n34);
+        EditText n41 = (EditText)findViewById(R.id.n41);
+        EditText n42 = (EditText)findViewById(R.id.n42);
+        EditText n43 = (EditText)findViewById(R.id.n43);
+        EditText n44 = (EditText)findViewById(R.id.n44);
+
+        TextView t1 = (TextView)findViewById(R.id.result);
+
+        int[][] multi = new int[4][4];
+
+
+        int a11 = Integer.parseInt(n11.getText().toString());
+        int a12 = Integer.parseInt(n1.getText().toString());
+        int a13 = Integer.parseInt(n1.getText().toString());
+        int a14 = Integer.parseInt(n1.getText().toString());
+        int a21 = Integer.parseInt(n1.getText().toString());
+        int a22 = Integer.parseInt(n1.getText().toString());
+        int a23 = Integer.parseInt(n1.getText().toString());
+        int a24 = Integer.parseInt(n1.getText().toString());
+        int a31 = Integer.parseInt(n1.getText().toString());
+        int a32 = Integer.parseInt(n1.getText().toString());
+        int a33 = Integer.parseInt(n1.getText().toString());
+        int a34 = Integer.parseInt(n1.getText().toString());
+        int a41 = Integer.parseInt(n1.getText().toString());
+        int a42 = Integer.parseInt(n1.getText().toString());
+        int a43 = Integer.parseInt(n1.getText().toString());
+        int a44 = Integer.parseInt(n1.getText().toString());
+
+
+        //t1.setText(Integer.toString(sum));
+
+
     }
 }
