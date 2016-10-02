b7ae9cec982388b6a7745669adf49c4ddcc58694
romalrj <romalrj07@gmail.com>
2016-10-02 09:59:37 +0530

    change 11

--- KILL_BOREDOM/app/src/main/java/rj/kill_boredom/page1211.java
+++ KILL_BOREDOM/app/src/main/java/rj/kill_boredom/page1211.java
@@ -1,15 +1,16 @@
 package rj.kill_boredom;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class page1211 extends AppCompatActivity {
 
-    EditText startPort, stopPort;
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +26,7 @@ public class page1211 extends AppCompatActivity {
         EditText n13 = (EditText)findViewById(R.id.n13);
         EditText n14 = (EditText)findViewById(R.id.n14);
         EditText n21 = (EditText)findViewById(R.id.n21);
-        /*EditText n22 = (EditText)findViewById(R.id.n22);
+        EditText n22 = (EditText)findViewById(R.id.n22);
         EditText n23 = (EditText)findViewById(R.id.n23);
         EditText n24 = (EditText)findViewById(R.id.n24);
         EditText n31 = (EditText)findViewById(R.id.n31);
@@ -35,16 +36,16 @@ public class page1211 extends AppCompatActivity {
         EditText n41 = (EditText)findViewById(R.id.n41);
         EditText n42 = (EditText)findViewById(R.id.n42);
         EditText n43 = (EditText)findViewById(R.id.n43);
-        EditText n44 = (EditText)findViewById(R.id.n44); */
+        EditText n44 = (EditText)findViewById(R.id.n44);
 
-        TextView t1 = (TextView)findViewById(R.id.result);
+
 
         int a11 = Integer.parseInt(n11.getText().toString());
         int a12 = Integer.parseInt(n12.getText().toString());
         int a13 = Integer.parseInt(n13.getText().toString());
         int a14 = Integer.parseInt(n14.getText().toString());
         int a21 = Integer.parseInt(n21.getText().toString());
-       /* int a22 = Integer.parseInt(n22.getText().toString());
+        int a22 = Integer.parseInt(n22.getText().toString());
         int a23 = Integer.parseInt(n23.getText().toString());
         int a24 = Integer.parseInt(n24.getText().toString());
         int a31 = Integer.parseInt(n31.getText().toString());
@@ -57,10 +58,24 @@ public class page1211 extends AppCompatActivity {
         int a44 = Integer.parseInt(n44.getText().toString());
 
 
-        */
-        t1.setText("aa aa");
+        if ((a11==4)&&(a21==1)&&(a31==3)&&(a41==2)&&(a12==2)&&(a22==3)&&(a32==4)&&(a42==1)&&(a13==1)&&(a23==4)&&(a33==2)&&(a43==3)&&(a14==3)&&(a24==2)&&(a34==1)&&(a44==4)){
+            Context context = getApplicationContext();
+            CharSequence text = "WELL DONE!";
+            int duration = Toast.LENGTH_SHORT;
 
-        //t1.setText(Integer.toString(sum));
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+        else
+        {
+            Context context = getApplicationContext();
+            CharSequence text = "INCORRECT. KEEP GOING..";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+
 
 
     }
